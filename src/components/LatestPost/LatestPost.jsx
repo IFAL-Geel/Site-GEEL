8201bff6b93544194544e70d2a93f82034b671de
Luscas-nl <eduardol123azq@gmail.com>
2023-11-14 05:06:49 -0300

    Design Update && API Test

--- src/components/LatestPost/LatestPost.jsx
+++ src/components/LatestPost/LatestPost.jsx
@@ -6,8 +6,7 @@ export default function LatestPost(props) {
         return(
             <div className="LatestPostInnerLarge">
                 <div className="lpTextsInner">
-                    <h3 className="lpTile">{props.title ?? "Lorem ipsum dolor sit amet consectetur adipiscing elit."}</h3>
-                    <p>{props.desc ?? "Proin aliquet sem quis tempor fermentum. In posuere dui vitae rhoncus tempus. In sed interdum odio, eget feugiat arcu. Fusce id ante non velit auctor semper. Integer gravida faucibus posuere. Maecenas varius risus eu odio ullamcorper porttitor. Nullam eu nibh a ipsum posuere vehicula quis in erat. Nulla at tincidunt velit, ac molestie libero."}</p>
+                    <h3 className="lpTile">{props.title != null ? props.title.slice(0 ,100) + "..." : "Lorem ipsum dolor sit amet consectetur adipiscing elit."}</h3>
                 </div>
                 <div className="lpImgInner">
                     <img src={props.img ?? "https://png.pngtree.com/thumb_back/fh260/background/20230523/pngtree-the-geometry-of-the-abstract-cubes-creates-a-futuristic-scene-image_2606442.jpg"} alt="" />
@@ -19,7 +18,7 @@ export default function LatestPost(props) {
         return(
             <div className="LatestPostInnerSmall">
                 <div className="lpTextsInner">
-                    <h3 className="lpTile">{props.title ?? "Lorem ipsum dolor sit amet consectetur adipiscing elit."}</h3>
+                    <h3 className="lpTile">{props.title != null ? props.title.slice(0 ,100) + "..." : "Lorem ipsum dolor sit amet consectetur adipiscing elit."}</h3>
                 </div>
                 <div className="lpImgInner">
                     <img src={props.img ?? "https://png.pngtree.com/thumb_back/fh260/background/20230523/pngtree-the-geometry-of-the-abstract-cubes-creates-a-futuristic-scene-image_2606442.jpg"} alt="" />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,27 +4,62 @@ import LatestPost from "../../components/LatestPost/LatestPost"
 import NewsPost from "../../components/NewsPost/NewsPost"
 import { Link } from "react-router-dom"
 import { Fade } from "react-awesome-reveal"
+import { useEffect, useState } from "react"
 
 export default function Home(props) {
-    return(
-        <div className="Home">
-            <PageDesc />
-                <div className="latestPostsInner">
-                <Fade triggerOnce="true">
-                    <LatestPost type="large" />
-                    <div className="lpSmallInner">
-                        <LatestPost type="small"/>
-                        <LatestPost type="small"/>
-                        <LatestPost type="small"/>
+
+    const [posts, setPosts] = useState(null)
+
+    const token = "IGQWRNNXA0TVl5S0VvX0RFMEFSVnhaSXl5M2VJekJvZAGZA2Q1dNZAjJPcG83ckVUeXdRRlFNZA1hTb1JCQnlHVnJwVVFyeThycmlLVkkxdUNGcFdMWUN5dUdtYTZAZAc0xMX1dtblJLdHU5TktYRFBYeTM4R3BrY0N0aGsZD";
+    const basicUrl = `https://graph.instagram.com/me/media?fields=caption,media_type,media_url&access_token=${token}&limit=7`;
+
+    useEffect(() =>{
+        console.log(posts);
+    }, [posts])
+
+    useEffect(() =>{
+        const fetchData = async () => {
+            try {
+                const response = await fetch(basicUrl)
+                const data = await response.json()
+                setPosts(data)
+            } catch (error) {
+                console.error("Erro na solicitação:", error)
+            }
+        }
+
+        if (posts === null) {
+            fetchData();
+        }
+    }, [basicUrl, posts])
+
+    if(!posts){
+        return(
+            <div className="Home">
+                <PageDesc />
+                    <div className="latestPostsInner">
+                    <Fade triggerOnce="true">
+                        <LatestPost type="large" img={posts != null ? posts?.data[1]?.media_url : null} title={posts != [] ? posts?.data[1]?.caption : null}/>
+                        <div className="lpSmallInner">
+                            <LatestPost type="small"/>
+                            <LatestPost type="small"/>
+                            <LatestPost type="small"/>
+                        </div>
+                        <NewsPost />
+                        <NewsPost />
+                        <NewsPost />
+                        </Fade>
                     </div>
-                    <NewsPost />
-                    <NewsPost />
-                    <NewsPost />
+                    <Fade className="homeFade" triggerOnce="true">
+                        <Link to="news">Mais Notícias</Link>
                     </Fade>
-                </div>
-                <Fade className="homeFade" triggerOnce="true">
-                    <Link to="news">Mais Notícias</Link>
-                </Fade>
-        </div>
-    )
+            </div>
+        )
+    } else {
+        return(
+            <div className="Home">
+                <div class="progress-4"></div>
+            </div>
+        )
+    }
 }
