69643639a8d1393d64c01436c32caae76cf1ef7b
Eliel Cirino <elielscirino@gmail.com>
2023-11-16 17:34:38 -0300

    About page and relatives

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,24 +2,38 @@ import { useState } from "react";
 
 import './About.css'
 import AboutCard from "../../components/AboutCard/AboutCard.jsx";
-import data from "../../data/diretorias.js";
+import PageTitle from "../../components/PageDesc/PageTitle.jsx";
+import logoGeel from "../../assets/images/LogoGEEL.png";
+import data from "../../data/aboutData/diretorias.js";
 
 export default function About(props) {
     const [index, setIndex] = useState(0);
 
     return(
-        <main className="About Main">
-            <div className="About__container">
-                <h2 className="About__title">Sobre nós</h2>
-                <div className="division__line"></div>
-                {/*general about text goes here with the logo*/}
-                <h2 className="About__title">Diretorias</h2>
-                <div className="division__line"></div>
-                <AboutCard
-                    content={data[index]}
-                    parentState={{current: index, setter: setIndex}}
-                />
+        <div className="About">
+            <PageTitle title="Sobre nós" />
+            <div className="About__geral">
+                <img src={logoGeel} alt="Logo do GEEL" />
+
+                <p>
+                    O <abbr title="Grêmio Estudantil Edson Luís">GEEL</abbr> orienta-se pelos princípios da defesa de
+                    uma educação pública, gratuita e de qualidade, a democracia como base para a participação estudantil,
+                    legalidade, transparência na gestão, gestão participativa, defesa dos interesses do corpo discente,
+                    autonomia e independência estudantil. Seus objetivos incluem fortalecer a identidade estudantil,
+                    promover cooperação na comunidade acadêmica, defender direitos e democracia, estimular a cidadania
+                    plena, valorizar a cultura e expressão criativa, contribuir para o desenvolvimento integral do corpo
+                    discente, buscar educação de qualidade, estabelecer parcerias e intercâmbios, manter
+                    representatividade ampliada, garantir participação e acessibilidade nos processos decisórios,
+                    promover democracia, transparência e acessibilidade no instituto, colaborar com a administração,
+                    promover saúde e bem-estar estudantil, incentivar responsabilidade ambiental e sustentabilidade,
+                    participar de movimentos sociais, e promover lazer e entretenimento.
+                </p>
             </div>
-        </main>
+            <PageTitle title="Diretorias" />
+            <AboutCard
+                content={data[index]}
+                parentState={{current: index, setter: setIndex}}
+            />
+        </div>
     )
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../pages/Home/Home"
 import Menu from "../components/Menu/Menu"
 import News from "../pages/News/News"
+import About from "../pages/About/About"
 
 export default function AppRoutes(props) {
     return(
@@ -10,6 +11,7 @@ export default function AppRoutes(props) {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/news" element={<News />} />
+                <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
     )
